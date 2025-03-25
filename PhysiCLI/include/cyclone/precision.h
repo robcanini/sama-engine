40add2b4147e13632a852e08dc793080ee9c949f
Roberto Canini <cyberleonsama@gmail.com>
2025-03-25 21:00:44 +0100

    add basic vector operator overloads and funcs

---
+++ PhysiCLI/include/cyclone/precision.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace cyclone {
+
+	/** Defines the precision of the square root operator. */
+	#define real_sqrt sqrtf
+
+	/** Defines a real number precision. By default single precision is provided. */
+	typedef float real;
+}
