bcdfc412131bca333987aabb4dc6ae4581117496
Roberto Canini <cyberleonsama@gmail.com>
2025-03-28 21:24:17 +0100

    apply vulkan validation layer

--- include/core/game_instance.h
+++ include/core/game_instance.h
@@ -1,20 +1,46 @@
+#pragma once
+
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
 #include <iostream>
 #include <stdexcept>
+#include <vector>
+#include <cstring>
 #include <cstdlib>
 
 namespace core {
+
+    const uint32_t WIDTH = 800;
+    const uint32_t HEIGHT = 600;
+
+    const std::vector<const char*> validationLayers = {
+        "VK_LAYER_KHRONOS_validation"
+    };
+
+    #ifdef NDEBUG
+        const bool enableValidationLayers = false;
+    #else
+        const bool enableValidationLayers = true;
+    #endif
 
     class GameInstance
     {
 
     public:
-        void run();
+        void run()
+        {
+            initWindow();
+            initVulkan();
+            mainLoop();
+            cleanup();
+        }
 
-    private:
-        void initVulkan();
+        void initVulkan()
+        {
+            createInstance();
+            setupDebugMessenger();
+        }
 
         void createInstance();
 
@@ -24,9 +50,21 @@ namespace core {
 
         void cleanup();
 
+        void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo);
+
+        void setupDebugMessenger();
+
+        std::vector<const char*> getRequiredExtensions();
+
+        bool checkValidationLayerSupport();
+
+        static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, VkDebugUtilsMessageTypeFlagsEXT messageType, const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData, void* pUserData);
+
     private:
         GLFWwindow* window;
 
         VkInstance instance;
+
+        VkDebugUtilsMessengerEXT debugMessenger;
     };
 }
--- src/physics/particle.cpp
+++ src/physics/particle.cpp
@@ -2,36 +2,36 @@
 
 #include "physics/particle.h"
 
+namespace physics {
 
-using namespace physics;
+	void Particle::integrate(real duration)
+	{
+		assert(duration > 0);
 
-void Particle::integrate(real duration)
-{
-	assert(duration > 0);
+		// Update linear position.
+		position.addScaledVector(velocity, duration);
 
-	// Update linear position.
-	position.addScaledVector(velocity, duration);
+		// Work out the acceleratino from the force.
+		Vector3 resultingAcc = acceleration;
+		resultingAcc.addScaledVector(forceAccum, inverseMass);
 
-	// Work out the acceleratino from the force.
-	Vector3 resultingAcc = acceleration;
-	resultingAcc.addScaledVector(forceAccum, inverseMass);
+		// Update linear velocity from the acceleration.
+		velocity.addScaledVector(resultingAcc, duration);
 
-	// Update linear velocity from the acceleration.
-	velocity.addScaledVector(resultingAcc, duration);
+		// Impose drag.
+		velocity *= real_pow(damping, duration);
 
-	// Impose drag.
-	velocity *= real_pow(damping, duration);
+		// Clear the forces.
+		clearAccumulator();
+	}
 
-	// Clear the forces.
-	clearAccumulator();
-}
+	void Particle::clearAccumulator()
+	{
+		forceAccum.clear();
+	}
 
-void Particle::clearAccumulator()
-{
-	forceAccum.clear();
-}
-
-void Particle::addForce(const Vector3& force)
-{
-	forceAccum += force;
+	void Particle::addForce(const Vector3& force)
+	{
+		forceAccum += force;
+	}
 }
